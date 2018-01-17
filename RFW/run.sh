1fd6869e1531e5fb697a27ec88c37e8773848419
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-17 18:32:30 +0530

    Update run.sh

--- RFW/run.sh
+++ RFW/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 count=0
-while ! nc -z cpservice 8080 
+while ! nc -z jboss 8080 
 do
  if [ $count -le 180 ] 
  then
@@ -11,7 +11,7 @@ do
 done
 echo connected
 if [ $count -le 180 ]; then
-   sh /robot/robot.sh cpserver 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
+   sh /robot/robot.sh jboss 8080 cicd_sample/cicd_sample@52.67.80.187:1521/GGKF
 else
    exit 1
 fi
