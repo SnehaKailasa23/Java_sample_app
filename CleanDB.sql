7f0d77fcdd0de5dc585170c00a5b985fcda4c4f8
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-17 12:01:48 +0530

    Update CleanDB.sql

--- CleanDB.sql
+++ CleanDB.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(*) INTO cnt FROM user_tables WHERE table_name = 'USERS';
+SELECT COUNT(*) FROM user_tables WHERE table_name = 'USERS';
 
-SELECT COUNT(*) INTO cnt FROM user_tables WHERE table_name = 'USERS';
+SELECT COUNT(*) FROM user_tables WHERE table_name = 'USERS';
 
