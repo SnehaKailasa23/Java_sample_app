f616785a3d3620079b5479965b6ef7eb630bc2d8
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-17 12:05:36 +0530

    Update CleanDB.sql

--- CleanDB.sql
+++ CleanDB.sql
@@ -1,6 +1,9 @@
 DECLARE
    countVar NUMBER;
 SELECT COUNT(*) INTO countVar FROM user_tables WHERE table_name = 'USERS';
+IF countVar <> 0 THEN
+      DROP TABLE USERS;
+    END IF;
 
 SELECT COUNT(*) INTO countVar FROM user_tables WHERE table_name = 'USER_ROLE';
 
