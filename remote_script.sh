772f1e544a3e93a30a4476b6b52e6f9778dc7e7a
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-17 19:51:07 +0530

    Update remote_script.sh

--- remote_script.sh
+++ remote_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scp ubuntu@13.230.131.198:/var/lib/jenkins/workspace/Jboss_application/SpringMVCSecurityXML/target/SpringMVCSecurityXML.war jboss_domain/standalone/deployments/
+scp ubuntu@13.230.131.198:/var/lib/jenkins/workspace/Jboss_application@3/SpringMVCSecurityXML/target/SpringMVCSecurityXML.war jboss_domain/standalone/deployments/
