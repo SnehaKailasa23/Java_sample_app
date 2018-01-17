be290fb2c895f2728799bffbeb6cf0b9b882e826
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-17 14:59:40 +0530

    Update Execution_order.sh

--- Execution_order.sh
+++ Execution_order.sh
@@ -1,1 +1,2 @@
-cat {CleanDB.sql,CreateDB.sql,InsertDB.sql,SelectTables.sql}|$sqlplus p3base/p3base@padlcicdggk4.sw.fortna.net:1521/FORTNAWCS
+. ./Variables
+cat {CleanDB.sql,CreateDB.sql,InsertDB.sql,SelectTables.sql}|$sqlplus $db_user_name/$db_password@$db_server
