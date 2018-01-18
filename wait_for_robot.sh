ca67004b204c6506b42241affe16917dcf489c0e
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-18 16:21:34 +0530

    Update wait_for_robot.sh

--- wait_for_robot.sh
+++ wait_for_robot.sh
@@ -3,8 +3,16 @@ source .env
 Container_status=`docker ps | grep "$robot_container_name"`
 while [ -n "$Container_status" ]
 do
-  echo sleeping;
-  sleep 5s;
-  Container_status=`docker ps | grep "$robot_container_name"`
+  if [ $count -le 180 ] 
+  then
+    echo sleeping;
+    sleep 5s;
+    Container_status=`docker ps | grep "$robot_container_name"`
+  fi
 done;
 echo "connected"
+if [ $count -le 180 ]; then
+   docker stop $robot_container_name
+else
+   exit 1
+fi
