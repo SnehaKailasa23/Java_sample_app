dea4c84d745bfc7e71811ed8099ce7e509b5d6cb
SnehaKailasa23 <35054580+SnehaKailasa23@users.noreply.github.com>
2018-01-23 16:35:45 +0530

    Update Execution_order.sh

--- Db_Scripts/Execution_order.sh
+++ Db_Scripts/Execution_order.sh
@@ -1,1 +1,1 @@
-cat {CleanDB.sql,CreateDB.sql,InsertDB.sql,SelectTables.sql}|$sqlplus $1/$2@$3
+cat {Db_Scripts/CleanDB.sql,Db_Scripts/CreateDB.sql,Db_Scripts/InsertDB.sql,Db_Scripts/SelectTables.sql}|$sqlplus $1/$2@$3
